69c0f1008eeea06f17f77167511d264f57d81e70
Domagoj Zganec <domagoj.zganec1@gmail.com>
2022-06-26 19:53:55 +0200

    fix: overflow hidden container

--- src/common/components/Container.styled.js
+++ src/common/components/Container.styled.js
@@ -8,7 +8,6 @@ const StyledContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
 `;
 
 export const Container = ({ children }) => (
