fb58b521bd97621bdfabc885e7647633e0b06350
Domagoj Zganec <domagoj.zganec1@gmail.com>
2022-11-10 13:01:48 +0100

    setting up electron

---
+++ public/electron.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const { app, BrowserWindow, ipcMain } = require("electron");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    fullscreen: true,
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
+  });
+
+  // and load the index.html of the app.
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
+
+  return win;
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  mainWindow = createWindow();
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+
+ipcMain.on("close-me", (evt, arg) => {
+  app.quit();
+});
+
+ipcMain.on("fullscreen-click", (evt, arg) => {
+  mainWindow.setFullScreen(!mainWindow.isFullScreen());
+});
