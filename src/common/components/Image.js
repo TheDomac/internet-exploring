5810c84d8724bdff8f25628f0e044112909dd23c
Domagoj Zganec <domagoj.zganec1@gmail.com>
2022-11-15 14:59:13 +0100

    image shell

--- src/common/components/Image.js
+++ src/common/components/Image.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import { shell } from "electron";
 
 const StyledImage = styled.img`
   display: block;
   margin: 0 auto;
   max-width: 100%;
+  transition: 300ms;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(120%);
+  }
 `;
 
 const Image = ({ fileName, className }) => {
@@ -13,11 +20,20 @@ const Image = ({ fileName, className }) => {
     image = fileName;
   } else {
     try {
-      image = require(`../../images/${fileName}`);
+      image = require(`../../images/${fileName}`); // example: /static/media/0fa45b480cf079599522.png
     } catch (e) {}
   }
 
-  return <StyledImage className={className} src={image} alt="No Image" />;
+  const handleClick = () => {
+    const isLocalImage = image.startsWith("/static") // set in require statement
+    const isLink = image.startsWith("https")
+    
+    if (isLocalImage || isLink) {
+      shell.openExternal(`https://internetexploring.io/images/${encodeURIComponent(image)}`);
+
+    }
+  }
+  return <StyledImage className={className} onClick={handleClick} src={image} alt="No Image" />;
 };
 
 export default Image;
