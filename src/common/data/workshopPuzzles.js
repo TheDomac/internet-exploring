71b7b7e1697ba0b5be972c41ce289852127fa7c2
Domagoj Zganec <domagoj.zganec1@gmail.com>
2022-12-08 21:42:01 +0100

    workshop emoji movies

--- src/common/data/workshopPuzzles.js
+++ src/common/data/workshopPuzzles.js
@@ -1,5 +1,11 @@
 const workshopPuzzles = [
   {
+    id: "vF94jf72ks0127nQNqfr",
+    name: "Emoji titles",
+    userNickname: "LucPuc",
+    userSocialMediaURL: "",
+  },
+  {
     id: "muWUrHfxMSJVEUt00fug",
     name: "A note of achievement",
     userNickname: "TheDomac",
